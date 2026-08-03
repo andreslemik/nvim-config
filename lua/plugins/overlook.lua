9a19f03c30be6f65adf543421d3a7cd4f61ffbba
Andrew Svidersky <andreslemik@yandex.ru>
2026-08-03 23:47:32 +0400

    overlook plugin: additional hotkeys

--- lua/plugins/overlook.lua
+++ lua/plugins/overlook.lua
@@ -25,5 +25,54 @@ return {
       end,
       desc = "Overlook: Restore popup",
     },
+    {
+      "<leader>pp",
+      function()
+        require("overlook.api").peek_cursor()
+      end,
+      desc = "Overlook: Peek cursor",
+    },
+    {
+      "<leader>pU",
+      function()
+        require("overlook.api").restore_all_popups()
+      end,
+      desc = "Overlook: Restore all popups",
+    },
+    {
+      "<leader>pf",
+      function()
+        require("overlook.api").switch_focus()
+      end,
+      desc = "Overlook: switch focus",
+    },
+    {
+      "<leader>ps",
+      function()
+        require("overlook.api").open_in_split()
+      end,
+      desc = "Overlook: Open popup in split",
+    },
+    {
+      "<leader>pv",
+      function()
+        require("overlook.api").open_in_vsplit()
+      end,
+      desc = "Overlook: Open popup in vsplit",
+    },
+    {
+      "<leader>pt",
+      function()
+        require("overlook.api").open_in_tab()
+      end,
+      desc = "Overlook: Open popup in tab",
+    },
+    {
+      "<leader>po",
+      function()
+        require("overlook.api").open_in_original_window()
+      end,
+      desc = "Overlook: Open popup in current window",
+    },
   },
 }
